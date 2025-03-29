b5e82615a51020d5cf5020190b065b6a4490ab1b
MarMiK-04 <marmikvasava051404@gmail.com>
2025-03-29 15:37:23 +0530

    routing path and navbar

--- Front-end/src/App.jsx
+++ Front-end/src/App.jsx
@@ -2,13 +2,31 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import{ createBrowserRouter,RouterProvider } from 'react-router-dom'
+
+import Home from './Components/Home'
+import About from './Components/About'
+import Contact from './Components/Contact'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const router = createBrowserRouter([
+    {
+      path:"/",
+      element:<Home/>
+    },
+    {
+      path :"/about",
+      element:<About/>
+    },
+    {
+      path :"/contact",
+      element:<Contact/>
+    }
+  ])
 
   return (
     <>
-    
+      <RouterProvider router={router}/>
     </>
   )
 }
